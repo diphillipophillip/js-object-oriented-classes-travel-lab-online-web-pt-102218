122851006314d334958aa75166fc099dff7be344
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-27 15:39:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ class Driver {
     this.name = name 
     this.startDate = new Date( startDate );
   }
- 
-   
- }
+ yearsExperienceFromBeginningOf( year ) {
+    let endDate = new Date( year, 1, 1 );
+    let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 );
+    return parseInt( totalYears );
+  }
 }
 
 class Route {
